08b064480621c5e1eb1693adb150de497e5f695b
Zayden <zaylanthier@gmail.com>
2026-03-19 11:14:29 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here!
+async function getPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+  displayPosts(posts);
+}
+
+function displayPosts(posts) {
+  const ul = document.getElementById("post-list");
+
+  posts.forEach((post) => {
+    const li = document.createElement("li");
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+
+    h1.textContent = post.title;
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    ul.appendChild(li);
+  });
+}
+
+getPosts();
